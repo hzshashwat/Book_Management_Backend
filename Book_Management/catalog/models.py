d49439a4604ed7fda4c2fe60d01a8d2e02c365ba
Shashwat Singh <shashwatsingh741@gmail.com>
2022-11-06 20:52:46 +0530

    Added models.py and ran migrations

--- Book_Management/catalog/models.py
+++ Book_Management/catalog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.core.validators import  MaxLengthValidator,  MinLengthValidator
 
 # Create your models here.
+class Book(models.Model):
+    name = models.CharField(max_length = 200)
+    isbn_no = models.CharField('ISBN', unique = True, primary_key = True, max_length = 13, validators=[ MaxLengthValidator(13 , message="As per ISBN standard, all ISBNs are 13-digits long."), MinLengthValidator(13, message="As per ISBN standard, all ISBNs are 13-digits long.")])
+    author_name = models.CharField(max_length = 150)
+    genre = models.CharField(max_length = 100)
+    inventory = models.IntegerField(default = 0)
