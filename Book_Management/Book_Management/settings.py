3f9d214f83eabd92d14dd182be83ab8dbc01350e
Shashwat Singh <shashwatsingh741@gmail.com>
2022-11-06 20:09:16 +0530

    Installed catalog to settings and routing setup to urls.py

--- Book_Management/Book_Management/settings.py
+++ Book_Management/Book_Management/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'catalog.apps.CatalogConfig',
     'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
