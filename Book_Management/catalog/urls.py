47aebbf010488248850e008982c1072977ec3bed
Shashwat Singh <shashwatsingh741@gmail.com>
2022-11-07 03:12:41 +0530

    built NeededBooksView and UnavailableBooksView

--- Book_Management/catalog/urls.py
+++ Book_Management/catalog/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from catalog.views import *
 
 urlpatterns = [
-    path('books/', HomeView, name = 'homeview')
+    path('books/', HomeView, name = 'homeview'),
+    path('books/find_books_needed/', NeededBooksView, name= 'neededbooks'),
+    path('books/unavailable_books/', UnavailableBooksView, name= 'unavailablebooks'),
+    path('book/', BookView, name= 'bookview')
 ]
--- Book_Management/catalog/views.py
+++ Book_Management/catalog/views.py
@@ -39,3 +39,60 @@ def HomeView(request):
             return Response({"message" : "All the book records have been deleted successfully."})
         except Exception as e:
             return({"error":e})
+
+@api_view(['GET'])
+def NeededBooksView(request):
+    if request.method == 'GET' :
+        limit = int(request.query_params['n'])
+        print(limit)
+        book = Book.objects.all().filter(inventory__lt = limit)
+        try:
+            bookjson = BookSerializers(book, many=True)
+            return Response({"message" : [bookjson.data]})
+        except Exception as e:
+            return({"error":e})
+
+@api_view(['GET'])
+def UnavailableBooksView(request):
+    if request.method == 'GET' :
+        book = Book.objects.all().filter(inventory = 0)
+        try:
+            bookjson = BookSerializers(book, many=True)
+            return Response({"message" : [bookjson.data]})
+        except Exception as e:
+            return({"error":e})
+
+@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
+def BookView(request):
+    if request.method == 'GET' :
+        book = Book.objects.get(isbn_no = request.query_params['isbn_no'])
+        try:
+            bookjson = BookSerializers(book, many=False)
+            return Response({"message" : [bookjson.data]})
+        except Exception as e:
+            return({"error":e})
+
+    elif request.method == 'PUT' :
+        #bookdata = request.data
+        try :
+            bookdata = request.data
+            print(bookdata)
+            bookobj = BookSerializers(data = bookdata)
+            if bookobj.is_valid() == True:
+                bookobj.save()
+                return Response({"message": "Your data is saved successfully",
+                "status": "Success"
+                })
+            else :
+                return Response({"message": bookobj.errors,
+                "status": "Failed"
+                })
+        except Exception as e:
+            return Response({"message": e})
+
+    elif request.method == 'DELETE' :
+        try:
+            book = Book.objects.all().delete()
+            return Response({"message" : "All the book records have been deleted successfully."})
+        except Exception as e:
+            return({"error":e})
