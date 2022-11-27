4521ce39bbd2f047c3c4f09834b00aba3be58742
Shashwat Singh <shashwatsingh741@gmail.com>
2022-11-27 16:05:48 +0530

    Routings changed

--- Book_Management/catalog/urls.py
+++ Book_Management/catalog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from catalog.views import *
 
 urlpatterns = [
-    path('books/', HomeView, name = 'homeview'),
+    path('', HomeView, name = 'homeview'),
     path('books/find_books_needed/', NeededBooksView, name= 'neededbooks'),
     path('books/unavailable_books/', UnavailableBooksView, name= 'unavailablebooks'),
     path('book/', BookView, name= 'bookview'),
